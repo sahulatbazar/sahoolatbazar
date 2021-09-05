bb3eca18bce4396299a1ff160ef4808672617c36
sahulatbazar <90128846+sahulatbazar@users.noreply.github.com>
2021-09-05 00:32:18 -0700

    desine changes

--- lib/adminside/adminmainpage.dart
+++ lib/adminside/adminmainpage.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class AdminPage extends StatelessWidget {
-  const AdminPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
--- lib/userpages/homepage..dart
+++ lib/userpages/homepage..dart
@@ -1,12 +1,116 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomepPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    CollectionReference users =
+        FirebaseFirestore.instance.collection('products');
+
     return Scaffold(
-      body: Center(
-        child: Text('HOME PAGE'),
+      drawer: Drawer(),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: [
+            // AppBar(
+            //   backgroundColor: Colors.transparent,
+            //   elevation: 0,
+            //   iconTheme: IconThemeData(color: Colors.black),
+
+            // ),
+            Container(
+              width: double.infinity,
+              color: Colors.pink,
+              height: 200,
+              child: Row(
+                children: [
+                  IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.menu,
+                      )),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: 'sasda',
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.search),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              height: 200,
+              child: StreamBuilder(
+                  stream: FirebaseFirestore.instance
+                      .collection('products')
+                      .snapshots(),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<QuerySnapshot> snapshot) {
+                    if (!snapshot.hasData) {
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                    return ListView(
+                      children: snapshot.data.docs
+                          .map((e) => Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                    alignment: Alignment.center,
+                                    color: Colors.blue.shade600,
+                                    child: Column(
+                                      children: [
+                                        Row(
+                                          children: [
+                                            Text('product name :'),
+                                            Text(e['name']),
+                                          ],
+                                        ),
+                                        Row(
+                                          children: [
+                                            Text('product price :'),
+                                            Text(e['price']),
+                                          ],
+                                        ),
+                                        Row(
+                                          children: [
+                                            Text('product quantity :'),
+                                            Text(e['quanity']),
+                                          ],
+                                        ),
+                                      ],
+                                    )),
+                              ))
+                          .toList(),
+                    );
+                  }),
+            ),
+          ],
+        ),
       ),
+      //     body: FutureBuilder<DocumentSnapshot>(
+      //   future: users.doc().get(),
+      //   builder:
+      //       (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+      //     if (snapshot.hasError) {
+      //       return Text("Something went wrong");
+      //     }
+
+      //     if (snapshot.hasData && !snapshot.data!.exists) {
+      //       return Text("Document does not exist");
+      //     }
+
+      //     if (snapshot.connectionState == ConnectionState.done) {
+      //       Map<String, dynamic> data =
+      //           snapshot.data!.data() as Map<String, dynamic>;
+      //       return Text("name: ${data['name']} ${data['quanity']}");
+      //     }
+
+      //     return Text("loading");
+      //   },
+      // )
     );
   }
 }
